93e8f1d2eff8bf099eb8f3a49eafe242d1f6417c
Pavel <p-olhovoi@bk.ru>
2023-06-07 19:10:06 +0200

    fix: Change the port of localhost

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use('/api', router)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
