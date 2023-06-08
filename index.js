f9f18e012dd9d640aeb48364e46e1335035dd10b
Pavel <p-olhovoi@bk.ru>
2023-06-08 11:32:11 +0200

    feat: Allow Access-Control-Allow-Credentials

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     next();
   });
 
